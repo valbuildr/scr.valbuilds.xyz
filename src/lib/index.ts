2d565892a7d67882aecfb3861643ca273ef707d1
Valentine <v4lbuild3r@gmail.com>
2025-06-01 13:57:23 -0500

    Add status badges to projects

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -2,20 +2,24 @@
 
 export const PROJECTS: {
   title: string;
+  status: "draft" | "alpha" | "beta" | "released" | "eol";
   buttonText: string;
   href: string;
   bgImage: string;
   darkBg: boolean;
 }[] = [
-  // {
-  //   title: "Transit Sitcombe (it's somewhat related to SCR, trust me)",
-  //   buttonText: "Learn more",
-  //   href: "https://sitcombe.valbuilds.xyz/",
-  //   bgImage: "/sitcombe.png",
-  //   darkBg: true,
-  // },
+  {
+    title: "Transit Sitcombe (it's somewhat related to SCR, trust me)",
+    status: "draft",
+    buttonText: "Learn more",
+    // href: "https://sitcombe.valbuilds.xyz/",
+    href: "/sitcombe",
+    bgImage: "/sitcombe.png",
+    darkBg: true,
+  },
   {
     title: "Shift Bot",
+    status: "alpha",
     buttonText: "Learn more/Get started",
     href: "/shift-bot",
     bgImage: "/shift.png",
@@ -23,6 +27,7 @@ export const PROJECTS: {
   },
   {
     title: "Platform Sign Redesign",
+    status: "released",
     buttonText: "View the designs",
     href: "/platform-sign-redesign",
     bgImage: "/ltc.png",
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -22,6 +22,37 @@
     class="mb-4 p-6 bg-cover max-w-screen-lg aspect-video bg-primary-700 border border-gray-600 lg:border-x border-x-0 text-white mx-auto lg:rounded-lg flex flex-col items-start justify-end relative"
     style={"background-image: url(" + project.bgImage + ");"}
   >
+    {#if project.status == "draft"}
+      <p
+        class="px-3 bg-yellow-500/50 border border-yellow-500 rounded-lg text-yellow-100 z-60 mb-2"
+      >
+        Draft
+      </p>
+    {:else if project.status == "alpha"}
+      <p
+        class="px-3 bg-blue-500/50 border border-blue-500 rounded-lg text-blue-100 z-60 mb-2"
+      >
+        Alpha
+      </p>
+    {:else if project.status == "beta"}
+      <p
+        class="px-3 bg-cyan-500/50 border border-cyan-500 rounded-lg text-cyan-100 z-60 mb-2"
+      >
+        Beta
+      </p>
+    {:else if project.status == "released"}
+      <p
+        class="px-3 bg-green-500/50 border border-green-500 rounded-lg text-green-100 z-60 mb-2"
+      >
+        Released
+      </p>
+    {:else if project.status == "eol"}
+      <p
+        class="px-3 bg-red-500/50 border border-red-500 rounded-lg text-red-100 z-60 mb-2"
+      >
+        End-of-Life
+      </p>
+    {/if}
     <p class="text-3xl sm:text-4xl font-bold text-white mb-2 z-11">
       {project.title}
     </p>
